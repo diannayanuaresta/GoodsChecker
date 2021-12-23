28540a32ec76746247e42e0f076c24bac0b5b6b7
restadianna <yanuarestadianna@gmail.com>
2021-12-23 13:26:13 +0700

    db fix

--- goodschecker.sql
+++ goodschecker.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2021 at 08:34 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.4.21
+-- Waktu pembuatan: 23 Des 2021 pada 07.25
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang_in`
+-- Struktur dari tabel `barang_in`
 --
 
 CREATE TABLE `barang_in` (
@@ -34,24 +34,22 @@ CREATE TABLE `barang_in` (
   `price` int(11) NOT NULL,
   `quantity` int(5) NOT NULL,
   `keterangan` text NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` date NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `barang_in`
+-- Dumping data untuk tabel `barang_in`
 --
 
 INSERT INTO `barang_in` (`id`, `kode_barang`, `nama_barang`, `price`, `quantity`, `keterangan`, `created_at`, `updated_at`) VALUES
-(1, 'bahan mentah', 'kayu jati', 1000000, 75, 'pembelian bahan mentah langsung dari perhutani', '0000-00-00', '0000-00-00'),
-(4, 'bahan mentah', 'rotan premium', 1500000, 100, 'pembelian bahan kursi kursi rotan', '0000-00-00', '0000-00-00'),
-(5, 'bahan mentah', 'kayu rotan', 2000000, 150, 'bahan mentah kursi ruang tengah', '0000-00-00', '0000-00-00'),
-(6, 'bahan mentah', 'kayu mahoni class b', 10000000, 300, 'keperluan bahan pembuatan lemari pesanan hj. udin', '0000-00-00', '0000-00-00');
+(14, 'bahan mentah', 'Rotan Class A', 2000000, 11000, 'Pembelian Bahan kursi tamu', '2021-12-23 04:25:17', '2021-12-23 05:28:12'),
+(17, 'bahan mentah', 'Mahoni Class C', 2500000, 200, 'Pembelian bahan mentah untuk produksi pintu dibeli dari Toko Pak Syamsudin', '2021-12-23 05:57:55', '2021-12-23 06:01:27');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang_out`
+-- Struktur dari tabel `barang_out`
 --
 
 CREATE TABLE `barang_out` (
@@ -61,50 +59,66 @@ CREATE TABLE `barang_out` (
   `price` int(11) DEFAULT NULL,
   `quantity` int(5) NOT NULL,
   `keterangan` text NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` date NOT NULL DEFAULT current_timestamp()
+  `created_at` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `barang_out`
+-- Dumping data untuk tabel `barang_out`
 --
 
-INSERT INTO `barang_out` (`id`, `kode_barang`, `nama_barang`, `price`, `quantity`, `keterangan`, `created_at`, `updated_at`) VALUES
-(13, 'bahan mentah', 'kayu mahoni class b', 10000000, 200, 'keperluan bahan pembuatan lemari pesanan hj. udin', '0000-00-00', '0000-00-00'),
-(14, 'bahan mentah', 'kayu rotan', 2000000, 50, 'produksi kursi ruang tengah', '0000-00-00', '0000-00-00');
+INSERT INTO `barang_out` (`id`, `kode_barang`, `nama_barang`, `price`, `quantity`, `keterangan`, `created_at`) VALUES
+(20, 'bahan mentah', 'Mahoni Class C', 2500000, 50, 'Retur Pembelian karena bahan rusak, tidak bisa digunakan untuk produksi', '2021-12-23');
+
+--
+-- Trigger `barang_out`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_barang_in` BEFORE DELETE ON `barang_out` FOR EACH ROW begin
+UPDATE barang_in
+SET quantity = quantity + OLD.quantity WHERE
+nama_barang = OLD.nama_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `tr_barang_out` AFTER INSERT ON `barang_out` FOR EACH ROW begin
+UPDATE barang_in SET quantity = quantity - NEW.quantity WHERE nama_barang = NEW.nama_barang;
+END
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `barang_in`
+-- Indeks untuk tabel `barang_in`
 --
 ALTER TABLE `barang_in`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nama_barang` (`nama_barang`);
 
 --
--- Indexes for table `barang_out`
+-- Indeks untuk tabel `barang_out`
 --
 ALTER TABLE `barang_out`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `barang_in`
+-- AUTO_INCREMENT untuk tabel `barang_in`
 --
 ALTER TABLE `barang_in`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `barang_out`
+-- AUTO_INCREMENT untuk tabel `barang_out`
 --
 ALTER TABLE `barang_out`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
